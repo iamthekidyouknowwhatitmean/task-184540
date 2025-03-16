295bd69f911303c1b6e53e03ea4b58bb49f30def
Kantemir <truewarrior228@gmail.com>
2025-03-16 20:38:57 +0300

    Изменения в страницах О сервисе и Контакты

--- services/contacts/index.php
+++ services/contacts/index.php
@@ -2,48 +2,12 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Контакты");
 $APPLICATION -> AddChainItem($APPLICATION->GetTitle(),$APPLICATION->GetCurDir());
-?>
-<div class="site-section border-bottom">
-    <div class="container">
-    <div class="row align-items-center">
-        <div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
-        <?$APPLICATION->IncludeComponent(
-				"bitrix:main.include",
-				"",
-				Array(
-					"AREA_FILE_SHOW" => "page",
-					"AREA_FILE_SUFFIX" => "contacts_image",
-				)
-		);?>
-        </div>
-        <div class="col-md-5 ml-auto"  data-aos="fade-up" data-aos-delay="200">
-        <div class="site-section-title mb-3">
-            <h2><?$APPLICATION->IncludeComponent(
-				"bitrix:main.include",
-				"",
-				Array(
-					"AREA_FILE_SHOW" => "page",
-					"AREA_FILE_SUFFIX" => "contacts_title",
-				)
-		);?></h2>
-        </div>
-        <?$APPLICATION->IncludeComponent(
-				"bitrix:main.include",
-				"",
-				Array(
-					"AREA_FILE_SHOW" => "page",
-					"AREA_FILE_SUFFIX" => "contacts_text",
-				)
-		);?>
-        </div>
-    </div>
-    </div>
-</div>
-
-
-
-<?$APPLICATION->IncludeComponent(
+?><?$APPLICATION->IncludeComponent(
 	"bitrix:main.include",
 	"",
-Array()
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+	Array(
+		"AREA_FILE_SHOW" => "page",
+		"AREA_FILE_SUFFIX" => "contacts_info",
+	)
+);?>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
--- services/index.php
+++ services/index.php
@@ -3,51 +3,12 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("О сервисе");
 $APPLICATION -> AddChainItem($APPLICATION->GetTitle(),$APPLICATION->GetCurDir());
 ?>
-<div class="site-section border-bottom">
-	<div class="container">
-	<div class="row align-items-center">
-		<div class="col-md-6" data-aos="fade-up" data-aos-delay="100">
-		<?$APPLICATION->IncludeComponent(
-				"bitrix:main.include",
-				"",
-				Array(
-					"AREA_FILE_SHOW" => "page",
-					"AREA_FILE_SUFFIX" => "services_image",
-				)
-		);?>
-		</div>
-		<div class="col-md-5 ml-auto"  data-aos="fade-up" data-aos-delay="200">
-		<div class="site-section-title mb-3">
-			<h2><?$APPLICATION->IncludeComponent(
-				"bitrix:main.include",
-				"",
-				Array(
-					"AREA_FILE_SHOW" => "page",
-					"AREA_FILE_SUFFIX" => "services_title",
-				)
-			);?></h2>
-		</div>
-		<?$APPLICATION->IncludeComponent(
-				"bitrix:main.include",
-				"",
-				Array(
-					"AREA_FILE_SHOW" => "page",
-					"AREA_FILE_SUFFIX" => "services_text",
-				)
-		);?>
-		</div>
-	</div>
-	</div>
-</div>
 <?$APPLICATION->IncludeComponent(
 	"bitrix:main.include",
 	"",
 	Array(
 		"AREA_FILE_SHOW" => "page",
-		"AREA_FILE_SUFFIX" => "inc",
-		"EDIT_TEMPLATE" => ""
+		"AREA_FILE_SUFFIX" => "services_info"
 	)
 );?>
-
-
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
